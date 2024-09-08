213d0047a78dac60520d7d805f699282153faaa0
Martin Emch <memch@gmx.ch>
2024-09-08 20:03:25 +0200

    start a hw specific file

---
+++ Arduino/libraries/Hardware/src/ESP32-2432S024.h
@@ -0,0 +1,25 @@
+/* -----------------------------------------------------------------------------
+  Copyright (c) Martin Emch, CH - 6010 Kriens
+  ------------------------------------------------------------------------------
+  --------------------------------------------------------------------------- */
+// -----------------------------------------------------------------------------
+#if ESP_ARDUINO_VERSION_MAJOR != 2
+  #error use esp32 by Espressiv Systems 2.0.17 for now
+#endif
+#if ESP_ARDUINO_VERSION_MINOR != 0
+  #error use esp32 by Espressiv Systems 2.0.17 for now
+#endif
+#if ESP_ARDUINO_VERSION_PATCH != 17
+  #error use esp32 by Espressiv Systems 2.0.17 for now
+#endif
+// -----------------------------------------------------------------------------
+#if LVGL_VERSION_MAJOR != 8
+  #error use lvgl 8.3.11 for now
+#endif
+#if LVGL_VERSION_MINOR != 3
+  #error use lvgl 8.3.11 for now
+#endif
+#if LVGL_VERSION_PATCH !=11
+  #error use lvgl 8.3.11 for now
+#endif
+// -----------------------------------------------------------------------------
